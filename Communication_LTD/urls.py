42c5bed153d61d7e92baf86eb1806d63d66750b0
Yuval-Betito <yuval36811@gmail.com>
2024-12-01 12:10:31 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views  # **שינוי: ייבוא ה-views המובנים לשינוי סיסמה**
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/', include('users.urls')),
+
+    # **שינוי: נתיבים עבור שינוי סיסמה וסיום התהליך**
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
 ]
